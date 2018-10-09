d5202330847131e23e4fbbe1f664d673048e4482
Amit Patange <amitpatange88@gmail.com>
2018-10-09 18:38:54 +0530

    poly concept added

---
+++ oops/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    public class Employee
+    {
+        public string FirstName = "FN";
+        public string LastName = "LN";
+
+        public virtual void PrintName()
+        {
+            Console.WriteLine(FirstName + "   " + LastName);
+        }
+
+        public static void Run()
+        {
+            Employee e1 = new Employee();
+            Employee e2 = new FullTimeEmployee();
+            Employee e3 = new PartTimeEmployee();
+
+
+            e1.PrintName();
+            e2.PrintName();
+            e3.PrintName();
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public override void PrintName()
+        {
+            Console.WriteLine(FirstName + "   " + LastName + "  FullTime");
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public override void PrintName()
+        {
+            Console.WriteLine(FirstName + "   " + LastName + "  PartTime");
+        }
+    }
+}
--- oops/Program.cs
+++ oops/Program.cs
@@ -7,15 +7,25 @@ namespace oops
         static void Main(string[] args)
         {
             //all oops concepts are implemented here.
+            Program.RunVirtualOverideMethods();
         }
         
         /// <summary>
         /// This is an example Delegate. 
         /// </summary>
-        private void RunDelegate()
+        private static void RunDelegate()
         {
             //Delegate Example demo.
             TestDelegate.CallDelegate();
+        }
+
+        /// <summary>
+        /// Example of Virtual and Override methods. Basically we are doing polumorphism here.
+        /// A base class reference variable can contain the child class object and invoke methods of that.
+        /// </summary>
+        private static void RunVirtualOverideMethods()
+        {
+            Employee.Run();
         }
     }
 }
